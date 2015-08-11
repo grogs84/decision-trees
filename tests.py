eab5106e4a5052726687baa7d6b630d9cce28b45
Matt Grogan <johnmattgrogan@gmail.com>
2015-08-10 19:43:42 -0500

    edited priting the results

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from data_sets import iris, gini_data
-from functions import gini_impurity, gini_gain, feature_splits, split
+from functions import gini_impurity, gini_gain, feature_splits, split, distribution_from_array, np
 
 
 if __name__ == "__main__":
@@ -18,10 +18,17 @@ if __name__ == "__main__":
 		if gain > max_gain:
 			max_gain = gain
 			best_split = split_val
-			best_l = splits[0]
-			best_r = splits[1]
+			best_l = np.array(splits[0])
+			best_r = np.array(splits[1])
 
+	print"\n"
 	print "Best split value was: {} for a gini_gain of {}".format(best_split, max_gain)
 	print "\n"
-	print best_l
-	print best_r
+	l = distribution_from_array(best_l)
+	r = distribution_from_array(best_r)
+	print "left leaf count: {}".format(len(best_l))
+	print l
+	print "\n"
+	print "right leaf count: {}".format(len(best_r))
+	print r
+	print "\n"
